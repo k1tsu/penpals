ca41bcdf1af687ec0995a750855e8fc0b2b3be25
Pedro H. Windisch <pedrowindisch@gmail.com>
2020-03-19 21:06:02 -0300

    changed folder name.

--- src/client/i18n.js
+++ src/client/i18n.js
@@ -1,7 +1,7 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enJson from './i18n/en.json';
-import ptJson from './i18n/pt.json';
+import enJson from './translations/en.json';
+import ptJson from './translations/pt.json';
 
 i18n
   .use(initReactI18next)
