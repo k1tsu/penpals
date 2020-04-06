bf847e93076647b9fe0e445f4f42a67e89898f0b
Pedro H. Windisch <pedrowindisch@gmail.com>
2020-04-06 09:49:00 -0300

    Added sidebar to the Home route

---
+++ src/client/components/index.js
@@ -0,0 +1,5 @@
+import Sidebar from './sidebar/Sidebar.jsx';
+
+export {
+  Sidebar
+}
--- src/client/views/Home.js
+++ src/client/views/Home.js
@@ -1,23 +1,13 @@
-import React, { useEffect } from "react";
-import { connect } from "react-redux";
-import { useTranslation } from "react-i18next";
-import { fetchReceivedLetters } from "../redux/actions/lettersActions";
+import React from "react";
+import Wrapper from './styles';
+import { Sidebar } from "../components";
 
-const Home = ({ letters, auth, dispatch }) => {
-  const { t } = useTranslation();
-
+const Home = () => {
   return (
     <Wrapper>
-      <Header title={t("text.hello")} subtitle={t("text.hasLetters")} />
-      <Letters letters={letters} />
+      <Sidebar />
     </Wrapper>
   );
 };
 
-const mapStateToProps = state => ({
-  theme: state.theme,
-  letters: state.letters,
-  auth: state.auth
-});
-
-export default connect(mapStateToProps)(Home);
+export default Home;
