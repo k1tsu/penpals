f72f7bd8ae0b4b1fd5956c54b87f788fbcf32d17
Pedro H. Windisch <pedrowindisch@gmail.com>
2020-04-06 09:38:05 -0300

    Fix merge conflicts

--- src/client/views/routes.js
+++ src/client/views/routes.js
@@ -5,20 +5,12 @@ import {
   Switch
 } from 'react-router-dom';
 import Home from './Home';
-import LoginPage from './LoginPage';
-import CurrentLetter from './CurrentLetter';
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/">
         <Home />
-      </Route>
-      <Route exact path="/login">
-        <LoginPage />
-      </Route>
-      <Route exact path="/:letterId">
-        <CurrentLetter />
       </Route>
       <Route render={() => (<h2>test</h2>)} /> 
     </Switch>
