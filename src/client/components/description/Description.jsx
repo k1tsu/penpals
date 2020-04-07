322e7daf7f0fc1f56ffd3b8bc8a5608bd90d040c
Pedro H. Windisch <pedrowindisch@gmail.com>
2020-04-07 12:30:42 -0300

    Added framer-motion and text animations

--- src/client/components/description/Description.jsx
+++ src/client/components/description/Description.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import Text from "./styles";
+import { Text, TextWrapper } from "./styles";
+import { Frame } from "framer";
 
 /**
- * This element will render a 15px, monospaced text. 
+ * This element will render a 15px, monospaced text.
  * It works great for texts with lots of words and so.
  */
 const Description = ({ children }) => (
-  <Text>{ children }</Text>
+  <TextWrapper>
+    <Frame
+      initial={{ y: 15 * 1.2, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      overflow="hidden"
+      transition={{ ease: "fadeOut", duration: 0.6 }}
+      background=""
+      width="25%"
+      height="10%"
+    >
+      <Text>{ children }</Text>
+    </Frame>
+  </TextWrapper>
 );
 
 export default Description;
--- src/client/components/description/styles.jsx
+++ src/client/components/description/styles.jsx
@@ -1,11 +1,20 @@
 import styled from "styled-components";
 
+const TextWrapper = styled.div`
+  height: 17px;
+  overflow: hidden;
+  font-size: 15px;
+  margin-left: 5%;
+`;
+
 const Text = styled.h3`
   font-family: 'IBM Plex Mono', monospace;
   font-size: 15px;
   margin: 0;
-  margin-left: 5%;
   text-align: left;
 `;
 
-export default Text;
+export {
+  Text,
+  TextWrapper
+};
